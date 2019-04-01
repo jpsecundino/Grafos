e2a01bf735a8e828c51d78cf872a8a5861e38cc1
jpsecundino <jp.secundino@usp.br>
2019-04-01 13:38:40 -0300

    adição de pesos ao cálculo da similaridade e correção do parâmetro "similaridade por classe gramatical"

--- TAD/txt_sim2.c
+++ TAD/txt_sim2.c
@@ -11,7 +11,7 @@
 #define SIZE_TAG 20
 #define WORD_MAX 1000
 
-const double WEIGHT[] = {1, 1, 0, 2};
+const double WEIGHT[] = {1, 1, 2};
 
 enum Classes{
 	ADJ = 1,
@@ -233,25 +233,13 @@ int main(int argc, char const *argv[]){
 	
 	printf("Digite o nome do primeiro arquivo\n");
 	scanf("%s", file_name);
-
 	adj_array *seq1 = file_pasing(file_name, word_classes, t);
-	adj_array *seq2 = file_pasing(file_name, word_classes, t);
 
 	printf("Digite o nome do segundo arquivo\n");
 	scanf("%s", file_name);
-
-	for (int i = 0; i < seq1->size; ++i)
-	{
-		printf("%d ", seq1->adjacencies_by_class[i]);
-	}
-
-	printf("\n");
-	for (int i = 0; i < seq1->size; ++i)
-	{
-		printf("%d ", seq2->adjacencies_by_class[i]);
-	}
-
-	printf("\n");
+	adj_array *seq2 = file_pasing(file_name, word_classes, t);
+	
+	// printf("\n");
 
 
 	graph *g1_word = graph_from_sequence(seq1->adjacencies_by_word, seq1->size);
@@ -261,19 +249,18 @@ int main(int argc, char const *argv[]){
 	graph *g2_class = graph_from_sequence(seq2->adjacencies_by_class, seq2->size);
 
 	printf("\n");
-	double 	s1 = graph_vertex_similarity(g1_word,g2_word),
-			s2 = graph_edge_similarity(g1_word,g2_word),
-			s3 = graph_vertex_similarity(g1_class,g2_class),
-			s4 = graph_edge_similarity(g1_class,g2_class);
-	double s = (s1 * WEIGHT[0] + s2 * WEIGHT[1] + s3 * WEIGHT[2] + s4 * WEIGHT[3]) / (WEIGHT[0] + WEIGHT[1] + WEIGHT[2] + WEIGHT[3]);
-	printf("Similaridade %.3lf%%\n", s * 100);
+	double 	sim_wordv = graph_vertex_similarity(g1_word,g2_word),
+		sim_worde = graph_edge_similarity(g1_word,g2_word),
+		sim_classe = graph_edge_similarity(g1_class,g2_class);
+	double sim_mean = (sim_wordv * WEIGHT[0] + sim_worde * WEIGHT[1] + sim_classe * WEIGHT[2] ) / (WEIGHT[0] + WEIGHT[1] + WEIGHT[2]);
+	
 	printf("=======================================================================================\n");
 	//comparando pelas palavras
-	printf("Similaridade de palavras: %.3lf%%\n", graph_vertex_similarity(g1_word,g2_word)*100);
-	printf("Similaridade de conexões entre palavras: %.3lf%%\n", graph_edge_similarity(g1_word,g2_word)*100);
+	printf("Similaridade de palavras: %.3lf%%\n", sim_wordv*100);
+	printf("Similaridade de conexões entre palavras: %.3lf%%\n", sim_worde*100);
 	//comparando pelas classes
-	printf("Similaridade de classes gramaticais: %.3lf%%\n", graph_vertex_similarity(g1_class,g2_class)*100);
-	printf("Similaridade de conexões entre classes gramaticais: %.3lf%%\n", graph_edge_similarity(g1_class,g2_class)*100);
+	printf("Similaridade de conexões entre classes gramaticais: %.3lf%%\n", sim_classe*100);
+	printf("Similaridade média: %.3lf%%\n", sim_mean * 100);
 	printf("=======================================================================================\n");
 
 
