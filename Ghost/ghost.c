c0d77f9f8f0707089a5a9217e7acb090c16b1529
Marina Fontes Alcântara Machado <39441826+marinamachado@users.noreply.github.com>
2019-04-01 15:50:35 -0300

    Update ghost.c

--- Ghost/ghost.c
+++ Ghost/ghost.c
@@ -9,7 +9,13 @@ typedef struct _graph graph;
 struct _graph {
     int n, m;
 };
-
+/*      Imprime a matriz que simboliza o grafo e o local do fantasma e do pacman
+    Parametros:
+        -int n : numero de linhas
+        -int m : numero de colunas
+        -int x_jog, y_jog : coordenadas do pacman
+        -int x_fant, y_fant : coordenadas do fantasma
+*/
 void print_matrix(int n, int m, int x_jog, int y_jog, int x_fant, int y_fant) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -31,11 +37,25 @@ void print_matrix(int n, int m, int x_jog, int y_jog, int x_fant, int y_fant) {
     };
     nanosleep(&t, NULL);
 }
-
+/*      Identificar o sinal do numero 
+    Parametro:
+        -int n : numero a ser identificado
+    Retorno:
+        - 1 : se o numero for positivo
+        - -1: se o numero for negativo
+        - 0 : se o numero for 0
+*/
 int signal(int n) {
     return n > 0 ? 1 : n < 0 ? -1 : 0;
 }
 
+/*      Mover o pac man e o fantasma
+    Parametro:
+        - graph *g : 
+        - int i, j : coordenadas do pac man
+        - int ghost_i, ghost_j : coordenadas do fantasma
+    Sem retorno     
+*/
 void move(graph *g, int i, int j, int ghost_i, int ghost_j) {
     int d_x = ghost_i - i;
     int d_y = ghost_j - j;
@@ -84,6 +104,7 @@ void move(graph *g, int i, int j, int ghost_i, int ghost_j) {
 int main() {
     srand(time(0));
     int n, m, x_player, y_player, x_ghost, y_ghost;
+    // posicao inicial do pacman e do fantas a
     scanf("%d %d %d %d %d %d, ", &n, &m, &x_player, &y_player, &x_ghost, &y_ghost);
     graph g = {
         n,
